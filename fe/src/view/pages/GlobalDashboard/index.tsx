3adc11e467e2e3b306be780f7b3de617212be81b
felipemarq <felipeshay2000@gmail.com>
2025-02-17 09:01:29 -0300

    fix: :bug: fixed a bug in global dashboard

--- fe/src/view/pages/GlobalDashboard/index.tsx
+++ fe/src/view/pages/GlobalDashboard/index.tsx
@@ -73,7 +73,7 @@ const GlobalDashboard = () => {
             <div className="flex w-full flex-col">
               <main className="flex flex-1 flex-col gap-4 px-4 py-2 md:gap-8 ">
                 <StatboxContainer />
-                <div className="grid gap-4 md:gap-8 grid-cols-12 ">
+                <div className="grid gap-4 md:gap-8 grid-cols-12 auto-rows-[150px]">
                   <AverageBarChartCard />
                   <DaysNotRegisteredCard />
                   {!isChartDataEmpty && selectedDashboardView === "ALL" && (
