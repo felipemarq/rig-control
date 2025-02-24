a4661d1ebc503b475d5c748183ddf0288f72175e
felipemarq <felipeshay2000@gmail.com>
2025-02-24 14:49:48 -0300

    fix: :bug: minor fixes

--- fe/src/components/AppSidebar/NavMain.tsx
+++ fe/src/components/AppSidebar/NavMain.tsx
@@ -33,7 +33,7 @@ export function NavMain({
 }) {
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Inova</SidebarGroupLabel>
+      <SidebarGroupLabel>Menu</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item) => (
           <Collapsible
